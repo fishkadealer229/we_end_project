a2dd9baae77eb220d7258d6c830a664da1e308aa
fishkadealer229 <94692853+fishkadealer229@users.noreply.github.com>
2022-04-26 14:33:41 +0300

    Albert, what you done?

--- fsite.py
+++ fsite.py
@@ -7,7 +7,7 @@ from FDataBase import FDataBase
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 
-DATABASE = 'meetings_data_new.db'
+DATABASE = 'api/meetings_data.db'
 DEBUG = True
 SECRET_KEY = 'Albert123'
 
